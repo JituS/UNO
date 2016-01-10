a668361f2bad3edcb55d5f89a1fb24cb87cf3d0e
jitendrs <jitendrs@INjitendrs.local>
2016-01-10 20:12:05 +0530

    jitendra - logs function and favicon icon added
    What changes have you made?
    
    insert the favicon icon file in public and wrote a function which will
    print the all logs of the server
    
    Why do we need this change?
    
    it would be nice if we will able to track all the logs of the server
    
    What can this change impact?
    	new file:   .DS_Store
    	new file:   Tpdkdesign.net-Refresh-Cl-Symbols-Tips.ico
    	modified:   lib/controller.js
    	modified:   public/.DS_Store
    	modified:   public/css/.DS_Store
    	modified:   public/favicon.ico
    	modified:   server.js
    
     Changes not staged for commit:
    	modified:   server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 var http = require('http');
 var controller = require('./lib/controller.js');
 var Game = require('./lib/game.js');
@@ -5,4 +6,4 @@ controller = controller();
 var onStart = function(){
 	console.log('Game started in port 8000')
 };
-var server = http.createServer(controller).listen(process.env.PORT,onStart);
+var server = http.createServer(controller).listen(8000,onStart);
