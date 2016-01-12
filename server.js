326482f7e3c5eeb5932af684f4ee76cc26761d85
jitendrs <jitendrs@INjitendrs.local>
2016-01-12 10:52:21 +0530

    port

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ var Game = require('./lib/game.js');
 controller = controller();
 var onStart = function(){
 	console.log('Game started in port 8000')
-};
+}
 var server = http.createServer(controller).listen(process.env.PORT,onStart);
