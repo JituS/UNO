c8dd57f7e09e2cd1a5734f220235ae53c181adf1
jitendrs <jitendrs@INjitendrs.local>
2016-01-16 11:45:51 +0530

    port changed

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ var http = require('http');
 var controller = require('./lib/controller.js');
 var Game = require('./lib/game.js');
 controller = controller();
-var server = http.createServer(controller).listen(process.env.PORT || 5555);
+var server = http.createServer(controller).listen(OPENSHIFT_NODEJS_PORT || 3000);
